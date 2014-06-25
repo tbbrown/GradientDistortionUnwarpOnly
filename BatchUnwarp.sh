e4fc4ec49e42a6b09178121c10f747b432480ea0
Timothy Brown <tbbrown@wustl.edu>
2014-06-25 09:52:46 -0500

    Ignore subject specific scripts for grid engine

--- BatchUnwarp.sh
+++ BatchUnwarp.sh
@@ -9,6 +9,7 @@ Subjects="LS2009 LS2037 LS2043"
 for subj in ${Subjects} ; do
     echo "Submitting gradient unwarping job for subject ${subj}"
     cp GradientDistortionUnwarpOnly.sh subject_scripts/GradientDistortionUnwarpOnly.${subj}.sh
-    qsub -q hcp_standard.q -V subject_scripts/GradientDistortionUnwarpOnly.${subj}.sh --project=PipelineTest --subject=${subj} --outdir=/home/NRG/tbrown01/projects/Pipelines/GradientDistortionUnwarpOnly
+    qsub -q hcp_standard.q -V subject_scripts/GradientDistortionUnwarpOnly.${subj}.sh \
+        --project=PipelineTest --subject=${subj} --outdir=${PWD}
 done
   
