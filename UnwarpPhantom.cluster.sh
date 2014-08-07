fe2ea75e1eb826a7383539b178d24e95361e2104
Timothy Brown <tbbrown@wustl.edu>
2014-08-07 11:37:23 -0500

    Modified script name for unwarping the phantom using a cluster. Added script for unwarping pipeline sample data locally

--- UnwarpPhantom.cluster.sh
+++ UnwarpPhantom.cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#./GradientDistortionUnwarpScansOnly.sh --project=Skyra_QC --subject=ACR_J4660_20140721 --outdir=${PWD} > subject_scripts/UnwarpPhantom.out 2>&1
+. /nrgpackages/scripts/sge_setup.sh
 
 cp GradientDistortionUnwarpScansOnly.sh subject_scripts/GradientDistortionUnwarpScansOnly.Phantom.sh
 qsub -q hcp_standard.q -V subject_scripts/GradientDistortionUnwarpScansOnly.Phantom.sh \
