3114df4efcb7590100c4720cf924232f0cc3c168
Timothy Brown <tbbrown@wustl.edu>
2014-08-07 10:18:49 -0500

    Added script to unwarp some sample data to release with the HCP Pipelines

---
+++ UnwarpPipelineSampleData.cluster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. /nrgpackages/scripts/sge_setup.sh
+
+mkdir -p subject_scripts
+
+Subjects="100307"
+
+for subj in ${Subjects} ; do
+    echo "Submitting gradient unwarping job for subject ${subj}"
+    cp GradientDistortionUnwarpOnly.sh subject_scripts/GradientDistortionUnwarpOnly.${subj}.sh
+    qsub -q hcp_standard.q -V subject_scripts/GradientDistortionUnwarpOnly.${subj}.sh \
+        --project=HCP_500 --subject=${subj} --outdir=${PWD}
+done
+  
